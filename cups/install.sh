7d7fa3fb54cb7aa833968489885a2bca71af78f6
Philipp Arndt <2f.mail@gmx.de>
2019-03-15 12:34:33 +0100

    -

--- cups/install.sh
+++ cups/install.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
+sudo apt-get update
 sudo apt-get install -y cups printer-driver-gutenprint
 yes | sudo cp ./config/cupsd.conf /etc/cups/cupsd.conf
 yes | sudo cp ./config/printers.conf /etc/cups/printers.conf 
